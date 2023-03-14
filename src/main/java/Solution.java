2a792941ac9c167ac545d74210d46a7039543f07
LiquidPlummer <kyle.plummer@revature.com>
2023-03-14 11:45:20 -0400

    cleaning up

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,20 +1,8 @@
-import java.util.LinkedList;
-import java.util.List;
-
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> elementList = new LinkedList<Integer>();
-
-        elementList.add(nums[0]);
-
-        for(int i = 1; i < nums.length; i++) {
-            for(Integer element : elementList) {
-                if (element + nums[i] == target) {
-                    return new int[]{elementList.indexOf(element), i};
-                }
-            }
-            elementList.add(nums[i]);
-        }
+        /*
+        Solve problem here
+         */
         return null;
     }
 }
